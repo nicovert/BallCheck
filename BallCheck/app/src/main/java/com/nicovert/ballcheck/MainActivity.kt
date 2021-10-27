958aa92a0e52549a79597b8b9831abb4b7c36cc7
nicovert <12763808+nicovert@users.noreply.github.com>
2021-10-27 01:48:38 -0400

    v1.0
    Basic functioning volley calls and recycler.

--- BallCheck/app/src/main/java/com/nicovert/ballcheck/MainActivity.kt
+++ BallCheck/app/src/main/java/com/nicovert/ballcheck/MainActivity.kt
@@ -15,6 +15,7 @@ import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.*
 import androidx.preference.PreferenceManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.android.volley.Request
 import com.android.volley.RequestQueue
 import com.android.volley.Response
@@ -22,7 +23,9 @@ import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_about.*
+import kotlinx.android.synthetic.main.fragment_nba_now.*
 import org.json.JSONException
+import java.lang.Exception
 import java.text.DateFormat
 import java.text.ParseException
 import java.text.SimpleDateFormat
@@ -102,9 +105,19 @@ class MainActivity : AppCompatActivity() {
                 val urlBase = "https://data.nba.net/prod/v2/"
                 val urlScores = "/scoreboard.json"
 
-                val urlDate = SimpleDateFormat("yyyyMMdd").format(calendar.time)
-                val urlFull = urlBase + urlDate + urlScores
-                refreshNBA(urlFull)
+                if (calendar.get(Calendar.HOUR_OF_DAY) < 3 && stillActive(calendar, urlBase, urlScores)) {
+                    Log.d("stillActive: ", "loading yesterday")
+                    calendar.add(Calendar.DATE, -1)
+                    val urlDate = SimpleDateFormat("yyyyMMdd").format(calendar.time)
+                    val urlFull = urlBase + urlDate + urlScores
+                    refreshNBA(urlFull)
+                } else {
+                    calendar.add(Calendar.DATE, 1)
+                    val urlDate = SimpleDateFormat("yyyyMMdd").format(calendar.time)
+                    val urlFull = urlBase + urlDate + urlScores
+                    refreshNBA(urlFull)
+                }
+
             } else if (id == R.id.WNBANowFragment) {
                 Toast.makeText(this, "Coming Soon", Toast.LENGTH_SHORT).show()
             } else {
@@ -116,46 +129,91 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun refreshNBA(url: String) {
-        Log.d("rNBA: ", "entered")
+        //Log.d("rNBA: ", "entered")
         queue = Volley.newRequestQueue(this)
         val request = JsonObjectRequest(Request.Method.GET, url, null,
             Response.Listener {
                 Log.d("RefreshNBA: ", "Volley api call success")
                 try {
                     val gamesArr = it.getJSONArray("games")
+                    val nowList = ArrayList<NowItem>(gamesArr.length())
                     for (i in 0 until gamesArr.length()) {
                         val game = gamesArr.getJSONObject(i)
-                        val clock = game.getString("clock")
-                        if (clock == "") {
+                        val gameProgress = game.getInt("statusNum")
+                        var vScore = "-"
+                        var hScore = "-"
+//                      get clock
+                        var clock = game.getString("clock")
+//                      before game start, provide starting time and zero score
+                        if (gameProgress == 1) {
                             if (game.getBoolean("isStartTimeTBD")) {
-                                val localTime = "TBD"
+                                clock = "TBD"
                             } else {
                                 val startTime = game.getString("startTimeUTC")
-                                val localTime = timezone(startTime)
-                                //Log.d("time: ", localTime)
+                                clock = timezone(startTime)
                             }
-                            val vScore = "0"
-                            val hScore = "0"
+                        } else if (gameProgress == 3) {
+                            clock = "FINAL"
+                            val gamePeriods = game.getJSONObject("period")
+                            if (gamePeriods.getInt("current") > gamePeriods.getInt("maxRegular")) {
+                                clock = "FINAL/OT"
+                            }
                         }
+//                      get team info (name, tricode, logo) via ids and local string arrays
                         val vTeam = game.getJSONObject("vTeam")
                         val vID = vTeam.getString("teamId")
                         val hTeam = game.getJSONObject("hTeam")
                         val hID = hTeam.getString("teamId")
 
-                        val vTeamArrID = resources.getIdentifier("id$vID","string-array", packageName)
-                        val vTeamArr = resources.getStringArray(vTeamArrID)
-                        val hTeamArrID = resources.getIdentifier("id$hID","string-array", packageName)
-                        val hTeamArr = resources.getStringArray(hTeamArrID)
+                        var vTeamName = "Unknown"
+                        var hTeamName = "Unknown"
+                        var vTeamTri = "UKN"
+                        var hTeamTri = "UKN"
+                        try {
+                            val vTeamArrID = resources.getIdentifier("id$vID","array", packageName)
+                            //Log.d("stringarray", "id$vID")
+                            //Log.d("stringarray", "id$hID")
+                            //Log.d("stringarray", vTeamArrID.toString())
+                            val vTeamArr = resources.getStringArray(vTeamArrID)
+                            val hTeamArrID = resources.getIdentifier("id$hID","array", packageName)
 
-                        val vTeamName = vTeamArr[2]
-                        val vTeamTri = vTeamArr[3]
-                        val hTeamName = hTeamArr[2]
-                        val hTeamTri = hTeamArr[3]
+                            val hTeamArr = resources.getStringArray(hTeamArrID)
+                            vTeamName = vTeamArr[2].toLowerCase()
+                            vTeamTri = vTeamArr[3]
+                            hTeamName = hTeamArr[2].toLowerCase()
+                            hTeamTri = hTeamArr[3]
+                        } catch (e: Exception) {
+                            //Toast.makeText(this, "error", Toast.LENGTH_SHORT).show()
+                            Log.d("stringarray","catch")
+                        }
 
                         val vTeamLogoID = resources.getIdentifier("logo_$vTeamName","drawable", packageName)
                         val hTeamLogoID = resources.getIdentifier("logo_$hTeamName","drawable", packageName)
 
+//                      get scores (game in progress)
+                        if(gameProgress == 2 || gameProgress == 3) {
+                            vScore = vTeam.getString("score")
+                            hScore = hTeam.getString("score")
+                        }
+                        //set score separator icon according to winning team
+                        var dotID = resources.getIdentifier("ic_circle", "drawable", packageName)
+                        if (vScore > hScore) {
+                            //visiting team winning
+                            dotID = resources.getIdentifier("ic_arrow_left", "drawable", packageName)
+                        } else if (vScore < hScore) {
+                            //home team winning
+                            dotID = resources.getIdentifier("ic_arrow_right", "drawable", packageName)
+                        }
+
+                        // create item for recycler
+                        val nowGame = NowItem(vTeamLogoID, hTeamLogoID, dotID, clock, vTeamTri, hTeamTri, vScore, hScore)
+                        nowList += nowGame
                     }
+
+                    recycler_view.adapter = NowAdapter(nowList)
+                    recycler_view.layoutManager = LinearLayoutManager(this)
+                    recycler_view.setHasFixedSize(true)
+
                 } catch (e: JSONException) {
                     e.printStackTrace()
                 }
@@ -173,8 +231,44 @@ class MainActivity : AppCompatActivity() {
         val utcFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Locale.getDefault())
         utcFormat.timeZone = TimeZone.getTimeZone("UTC")
         val utc = utcFormat.parse(utcString)
-        val dateFormat = SimpleDateFormat("hh:mma z", Locale.getDefault())
+        val dateFormat = SimpleDateFormat("hh:mma", Locale.getDefault())
         dateFormat.timeZone = TimeZone.getDefault()
         return dateFormat.format(utc)
+    }
+
+    private fun stillActive(today: Calendar, urlBase: String, urlScores: String): Boolean {
+        Log.d("still active", "still active run")
+        today.add(Calendar.DATE, -1)
+        val urlDate = SimpleDateFormat("yyyyMMdd").format(today.time)
+        val url = urlBase + urlDate + urlScores
+        var result = false
+        queue = Volley.newRequestQueue(this)
+        val request = JsonObjectRequest(Request.Method.GET, url, null,
+            Response.Listener {
+                Log.d("RefreshNBA: ", "Volley api call success")
+                try {
+                    val gamesArr = it.getJSONArray("games")
+                    for (i in 0 until gamesArr.length()) {
+                        val game = gamesArr.getJSONObject(i)
+                        //val gameProgress = game.getInt("statusNum")
+                        val gameActive = game.getBoolean("isGameActivated")
+                        if (gameActive) {
+                            Log.d("still active: ", "found active game")
+                            result = true
+                            break
+                        }
+                    }
+                } catch (e: JSONException) {
+                    e.printStackTrace()
+                }
+            }, Response.ErrorListener {
+                Log.d("RefreshNBA: ", "Volley api call failed")
+                Log.d("RefreshNBA: ", it.toString())
+                Toast.makeText(this, "Network Error", Toast.LENGTH_SHORT).show()
+            })
+
+//        call request
+        queue.add(request)
+        return result
     }
 }
